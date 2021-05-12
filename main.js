a023638a6641831ce6c0e3cc1cfe02a2cb13916e
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-05-12 15:00:33 +0200

    resize images and test modal for products

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 // script
+$(document).ready(function(){
+  //pour afficher .collierMiLong
+  $("#categorie1").click(function(){
+    $(".bouclesOreilles, .tourDeCou, .dragon").toggle();
+  });
+  //pour afficher .pendentif
+  $("#categorie2").click(function(){
+    $(".bouclesOreilles,.tourDeCou, .collierMiLong").toggle();
+  });
+  //pour afficher .tourDeCou
+  $("#categorie3").click(function(){
+    $(".bouclesOreilles, .dragon, .collierMiLong").toggle();
+  });
+  //pour afficher .bouclesOreilles
+  $("#categorie4").click(function(){
+    $(".tourDeCou, .dragon, .collierMiLong").toggle();
+  });
+
+
+
+
+});
