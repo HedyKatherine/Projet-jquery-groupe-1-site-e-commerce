1e902155fbe6d79d9f7b87a149a2a7b484840a44
SophieeV <sophievesin@wanadoo.fr>
2021-05-12 10:13:35 +0200

    ajout photos

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    //Dès qu'on clique sur #b1, on applique hide() au texte
+    $("#categorie1").click(function(){
+        $(".bouclesOreilles, .tourDeCou").toggle();
+    });
+
+//var img = document.createElement(src="images/_DSC0001.JPG");
+
+
+
+/*
+$("#collier1").click(function(){
+  confirm("beleded"+"\n"+"dededqede")*/
+
+
+});
