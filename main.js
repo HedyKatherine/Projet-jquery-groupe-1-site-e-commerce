9d960c97f6fcdc985b03f10a97be6548ed46e9f2
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-05-21 01:48:25 +0200

    add art. to script-art disable dupl of crd17 in html and main.js and qty in scpanjs del stylmapcss com fr css

--- main.js
+++ main.js
@@ -1,226 +1,194 @@
-$(document).ready(function() {
-    //pour afficher colliers mi-longs
-    $("#categorie1").click(function() {
-        $(".collierMiLong").toggle();
-        $(".imageBouton").css("display", "none");
-        $(".textBouton").css("margin-top", "5px");
-        $(".textBouton").css("font-size", "20px");
-        $(".boutons").css("height", "20px");
-        $(".bouton").css("height", "20px");
-    });
-    $("#categorie2").click(function() {
-        $(".sautoir").toggle();
-        $(".imageBouton").css("display", "none");
-        $(".textBouton").css("margin-top", "5px");
-        $(".textBouton").css("font-size", "20px");
-        $(".boutons").css("height", "20px");
-        $(".bouton").css("height", "20px");
-    });
-    $("#categorie3").click(function() {
-        $(".tourDeCou").toggle();
-        $(".imageBouton").css("display", "none");
-        $(".textBouton").css("margin-top", "5px");
-        $(".textBouton").css("font-size", "20px");
-        $(".boutons").css("height", "20px");
-        $(".bouton").css("height", "20px");
-    });
-    $("#categorie4").click(function() {
-        $(".bouclesOreilles").toggle();
-        $(".imageBouton").css("display", "none");
-        $(".textBouton").css("margin-top", "5px");
-        $(".textBouton").css("font-size", "20px");
-        $(".boutons").css("height", "20px");
-        $(".bouton").css("height", "20px");
+$(document).ready(function(){
+  //pour afficher colliers mi-longs
+  $("#categorie1").click(function(){
+    $(".collierMiLong").toggle();
+    $(".imageBouton").css("display","none");
+    $(".textBouton").css("margin-top","5px");
+    $(".textBouton").css("font-size","20px");
+    $(".boutons").css("height","20px");
+    $(".bouton").css("height","20px");
+  });
+  $("#categorie2").click(function(){
+    $(".sautoir").toggle();
+    $(".imageBouton").css("display","none");
+    $(".textBouton").css("margin-top","5px");
+    $(".textBouton").css("font-size","20px");
+    $(".boutons").css("height","20px");
+    $(".bouton").css("height","20px");
+  });
+  $("#categorie3").click(function(){
+    $(".tourDeCou").toggle();
+    $(".imageBouton").css("display","none");
+    $(".textBouton").css("margin-top","5px");
+    $(".textBouton").css("font-size","20px");
+    $(".boutons").css("height","20px");
+    $(".bouton").css("height","20px");
+  });
+  $("#categorie4").click(function(){
+    $(".bouclesOreilles").toggle();
+    $(".imageBouton").css("display","none");
+    $(".textBouton").css("margin-top","5px");
+    $(".textBouton").css("font-size","20px");
+    $(".boutons").css("height","20px");
+    $(".bouton").css("height","20px");
 
-    });
+  });
 });
 
 /////pour le mouseover sur les photos des cartes
 //carte1
 function passage1(element) {
-    element.setAttribute('src', 'images/15.jpg');
+  element.setAttribute('src', 'images/15.jpg');
 }
-
 function departure1(element) {
-    element.setAttribute('src', 'images/14.jpg');
+  element.setAttribute('src', 'images/14.jpg');
 }
 //carte2
 function passage2(element) {
-    element.setAttribute('src', 'images/34.jpg');
+  element.setAttribute('src', 'images/34.jpg');
 }
-
 function departure2(element) {
-    element.setAttribute('src', 'images/35.jpg');
+  element.setAttribute('src', 'images/35.jpg');
 }
 //carte3
 function passage3(element) {
-    element.setAttribute('src', 'images/42.jpg');
+  element.setAttribute('src', 'images/42.jpg');
 }
-
 function departure3(element) {
-    element.setAttribute('src', 'images/43.jpg');
+  element.setAttribute('src', 'images/43.jpg');
 }
 //carte4
 function passage4(element) {
-    element.setAttribute('src', 'images/17.jpg');
+  element.setAttribute('src', 'images/17.jpg');
 }
-
 function departure4(element) {
-    element.setAttribute('src', 'images/18.jpg');
+  element.setAttribute('src', 'images/18.jpg');
 }
 //carte5
 function passage5(element) {
-    element.setAttribute('src', 'images/1.jpg');
+  element.setAttribute('src', 'images/1.jpg');
 }
-
 function departure5(element) {
-    element.setAttribute('src', 'images/50.jpg');
+  element.setAttribute('src', 'images/50.jpg');
 }
 //carte6
 function passage6(element) {
-    element.setAttribute('src', 'images/c.jpg');
+  element.setAttribute('src', 'images/c.jpg');
 }
-
 function departure6(element) {
-    element.setAttribute('src', 'images/b.jpg');
+  element.setAttribute('src', 'images/b.jpg');
 }
 //carte7
 function passage7(element) {
-    element.setAttribute('src', 'images/g.jpg');
+  element.setAttribute('src', 'images/g.jpg');
 }
-
 function departure7(element) {
-    element.setAttribute('src', 'images/i.jpg');
+  element.setAttribute('src', 'images/i.jpg');
 }
 //carte8
 function passage8(element) {
-    element.setAttribute('src', 'images/8.jpg');
+  element.setAttribute('src', 'images/8.jpg');
 }
-
 function departure8(element) {
-    element.setAttribute('src', 'images/9.jpg');
+  element.setAttribute('src', 'images/9.jpg');
 }
 //carte9
 function passage9(element) {
-    element.setAttribute('src', 'images/51.jpg');
+  element.setAttribute('src', 'images/51.jpg');
 }
-
 function departure9(element) {
-    element.setAttribute('src', 'images/52.jpg');
+  element.setAttribute('src', 'images/52.jpg');
 }
 //carte10
 function passage10(element) {
-    element.setAttribute('src', 'images/46.jpg');
+  element.setAttribute('src', 'images/46.jpg');
 }
-
 function departure10(element) {
-    element.setAttribute('src', 'images/47.jpg');
+  element.setAttribute('src', 'images/47.jpg');
 }
 //carte11
 function passage11(element) {
-    element.setAttribute('src', 'images/48.jpg');
+  element.setAttribute('src', 'images/48.jpg');
 }
-
 function departure11(element) {
-    element.setAttribute('src', 'images/49.jpg');
+  element.setAttribute('src', 'images/49.jpg');
 }
 //carte12
 function passage12(element) {
-    element.setAttribute('src', 'images/54.jpg');
+  element.setAttribute('src', 'images/54.jpg');
 }
-
 function departure12(element) {
-    element.setAttribute('src', 'images/55.jpg');
+  element.setAttribute('src', 'images/55.jpg');
 }
 //carte13
 function passage13(element) {
-    element.setAttribute('src', 'images/62.jpg');
+  element.setAttribute('src', 'images/62.jpg');
 }
-
 function departure13(element) {
-    element.setAttribute('src', 'images/60.jpg');
+  element.setAttribute('src', 'images/60.jpg');
 }
 //carte14
 function passage14(element) {
-    element.setAttribute('src', 'images/44.jpg');
+  element.setAttribute('src', 'images/44.jpg');
 }
-
 function departure14(element) {
-    element.setAttribute('src', 'images/45.jpg');
+  element.setAttribute('src', 'images/45.jpg');
 }
 //carte16
 function passage16(element) {
-    element.setAttribute('src', 'images/57.jpg');
+  element.setAttribute('src', 'images/57.jpg');
 }
-
 function departure16(element) {
-    element.setAttribute('src', 'images/58.jpg');
+  element.setAttribute('src', 'images/58.jpg');
 }
 //carte17
 function passage17(element) {
-    element.setAttribute('src', 'images/30.jpg');
+  element.setAttribute('src', 'images/30.jpg');
 }
-
 function departure17(element) {
-    element.setAttribute('src', 'images/31.jpg');
+  element.setAttribute('src', 'images/31.jpg');
 }
-//carte18
+/*
+// carte18 est un doublon de card 17
 function passage18(element) {
-    element.setAttribute('src', 'images/26.jpg');
+  element.setAttribute('src', 'images/26.jpg');
 }
-
 function departure18(element) {
-    element.setAttribute('src', 'images/27.jpg');
+  element.setAttribute('src', 'images/27.jpg');
 }
+*/
 //carte19
 function passage19(element) {
-    element.setAttribute('src', 'images/32.jpg');
+  element.setAttribute('src', 'images/32.jpg');
 }
-
 function departure19(element) {
-    element.setAttribute('src', 'images/33.jpg');
+  element.setAttribute('src', 'images/33.jpg');
 }
 //carte20
 function passage20(element) {
-    element.setAttribute('src', 'images/38.jpg');
+  element.setAttribute('src', 'images/38.jpg');
 }
-
 function departure20(element) {
-    element.setAttribute('src', 'images/39.jpg');
+  element.setAttribute('src', 'images/39.jpg');
 }
 //carte21
 function passage21(element) {
-    element.setAttribute('src', 'images/40.jpg');
+  element.setAttribute('src', 'images/40.jpg');
 }
-
 function departure21(element) {
-    element.setAttribute('src', 'images/41.jpg');
+  element.setAttribute('src', 'images/41.jpg');
 }
 //carte22
 function passage22(element) {
-    element.setAttribute('src', 'images/28.jpg');
+  element.setAttribute('src', 'images/28.jpg');
 }
-
 function departure22(element) {
-    element.setAttribute('src', 'images/29.jpg');
+  element.setAttribute('src', 'images/29.jpg');
 }
 //carte23
 function passage23(element) {
-    element.setAttribute('src', 'images/4.jpg');
+  element.setAttribute('src', 'images/4.jpg');
 }
-
 function departure23(element) {
-    element.setAttribute('src', 'images/5.jpg');
+  element.setAttribute('src', 'images/5.jpg');
 }
-
-// <!-- DONNEES DU LOCAL STORAGE -->
-// 	<!-- nom, id, img, prix, categorie -->
-
-
-// <!-- ajouter une donnée = .setItem(nomClé,valeurClé);
-//  récupérer une donnée = .session.getItem(clé); -->
-localStorage.setItem("nom", "collier Adenora");
-localStorage.setItem("id", "card1");
-localStorage.setItem("image", "images/image14.jpg");
-localStorage.setItem("prix", 20);
-localStorage.setItem("categorie", "collier");
---
+++ script-article.js
@@ -0,0 +1,36 @@
+function article(nom, id, image, prix, categorie) {
+  this.nom = nom;
+  this.id = id;
+  this.image = image;
+  this.prix = prix;
+  this.categorie = categorie;
+}
+
+listeArticle = [
+  new article("Collier Adenora", "card1", "images/14.jpg", 20, "colliers"),
+  new article("Boucles d'oreilles Dragon rouge", "card2", "images/34.jpg", 8.5, "bouclesOreilles"),
+  new article("Boucles d'oreilles chat", "card3", "images/14.jpg", 20, "colliers"),
+  new article("Collier Automne", "card4", "images/17.jpg", 27, "colliers"),
+  new article("Collier steampunk", "card5", "images/1.jpg", 15, "tourDeCou"),
+  new article("Collier Alvanna bronze", "card6", "images/c.jpg", 30, "sautoir"),
+  new article("Collier Alvanna argent", "card7", "images/g.jpg", 30, "sautoir"),
+  new article("Ras du cou Anaïk", "card8", "images/8.jpg", 10, "tourDeCou"),
+  new article("Ras du cou Serdaigle", "card9", "images/51.jpg", 20, "tourDeCou"),
+  new article("Boucles d'oreilles Serdaigle", "card10", "images/46.jpg", 5.50, "bouclesOreilles"),
+  new article("Boucles d'oreilles Serpentard", "card11", "images/48.jpg", 5.50, "bouclesOreilles"),
+  new article("Ras du cou Serpentard", "card12", "images/54.jpg", 9, "tourDeCou"),
+  new article("Ras du cou Poussoufle", "card13", "images/62.jpg", 9, "tourDeCou"),
+  new article("Boucles d'oreilles Poussoufle", "card14", "images/44.jpg", 5.50, "bouclesOreilles"),
+  new article("Boucles d'oreilles Griffondor", "card15", "images/63.jpg", 5.50, "bouclesOreilles"),
+  new article("Ras du cou Griffondor", "card16", "images/57.jpg", 9, "tourDeCou"),
+  new article("Boucles d'oreilles Bahiga", "card17", "images/30.jpg", 8, "bouclesOreilles"),
+  new article("Boucles d'oreilles Aggrabah", "card18", "images/26.jpg", 8.50, "bouclesOreilles"),
+  new article("Boucles d'oreilles Louise", "card19", "images/32.jpg", 7.50, "bouclesOreilles"),
+  new article("Boucles d'oreilles Bailee", "card20", "images/38.jpg", 7, "bouclesOreilles"),
+  new article("Boucles d'oreilles Baibre", "card21", "images/40.jpg", 8, "bouclesOreilles"),
+  new article("Collier Octopus", "card22", "images/28.jpg", 15, "collierMiLong")
+];
+/*
+// due to a duplicate of product <div class="card col-12 col-md-6 col-lg-3 bouclesOreilles" id="card17">
+// new article("nom", "id", "image", "prix", "categorie") was no longer necessary
+*/
